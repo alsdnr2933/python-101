74c0188fd0e5d0a303edd7fe7f9bb0937fee05ba
kim min uk <alsdnr2933@gmail.com>
2026-06-08 18:58:02 +0900

    lesson-05

---
+++ 2_practice.py
@@ -0,0 +1,509 @@
+# for i in range(7):
+#     print("Hello")
+
+# ------------------------------------------
+
+# count = 1
+# while count <=5:
+#     print(count)
+#     count += 1
+
+# ----------------------------------------
+
+# for i in range(0, 10):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(3, 9):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(0, 21, 2):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# ----------------------------------------
+
+# name = "Python"
+# for letter in name:
+#     print(letter)
+
+# ----------------------------------------
+
+# fruits = ["apple, banana, orange, grape"]
+# for fruit in fruits:
+#     print(fruit)
+
+# ----------------------------------------
+
+# for i in range(10):
+#     if i >= 6:
+#         break
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(10):
+#     if i == 4:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+
+# total = 0
+# for i in range(1, 11):
+#     total += i
+# print(total)
+
+# ----------------------------------------
+
+# for row in range(4):
+#     for col in range(5):
+#         print(f'row: {row}, col: {col}')
+
+# ----------------------------------------
+
+# for i in range(4):
+#     print("Hello")
+
+# ----------------------------------------
+
+# count = 1
+# while count < 8:
+#     print(count)
+#     count += 1
+
+
+# ----------------------------------------
+
+# for i in range(5, 13):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(1, 31, 3):
+#     print(i) 
+
+# ----------------------------------------
+
+# name = "KOREA"
+
+# for letter in name:
+#     print(letter)
+
+# ----------------------------------------
+
+# games = ['RPG, FPS, Action, Puzzle']
+
+# for game in games:
+#     print(game)
+
+# ----------------------------------------
+
+# for i in range(0, 10):
+#     if i >= 7:
+#         break
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(0, 10):
+#     if i == 3:
+#         continue
+#     if i == 6:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+
+# total = 0
+# for i in range(0, 21):
+#     total += i
+# print(total)
+
+# ----------------------------------------
+
+# for row in range(1, 3):
+#     for col in range(1, 4):
+#         print(f'row: {row}, col: {col}')
+
+# --------------------------------------------------------------------------------
+
+# for i in range(6):
+#     print("Study Python")
+
+# # ----------------------------------------
+
+# count = 1
+# while count <= 10:
+#     print(count)
+#     count += 1
+
+# ----------------------------------------
+
+# for i in range(7, 16):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(5, 51, 5):
+#     print(i)
+# ----------------------------------------
+
+# name = "JAPAN"
+# for i in name:
+#     print(i)
+
+# ----------------------------------------------------------------------------------------------------------------
+
+# job = ["Designer", "Programmer", "Planner", "Artist"]
+
+# for jobs in job:
+#     print(jobs)
+
+# ----------------------------------------
+
+# for i in range(0, 10):
+#     if i >= 4:
+#         break
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(0, 11):
+#     if i == 2:
+#         continue
+#     if i == 8:
+#         continue
+#     print(i)
+
+# --------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------
+
+# total = 0
+# for i in range(1, 31):
+#     total += i
+# print(total)
+
+# # ----------------------------------------------------------------------------------------------------------------------
+
+# for row in range(1, 5):
+#     for col in range(1, 3):
+#         print(f'row: {row}, col: {col}')
+
+
+
+
+# ============================================================================================================================
+
+# for letter in range(5):
+#     print("Hello World")
+
+# ----------------------------------------
+
+# for i in range(1, 11):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(3, 16):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(2, 21, 2):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(10, 0, -1):
+#     print(i)
+
+# ----------------------------------------
+
+# name = "PYTHON"
+
+# for i in name:
+#     print(i)
+
+# ----------------------------------------
+
+# Subjects = ["Math", "English", "Science", "History"]
+
+# for Subject in Subjects:
+#     print(Subject)
+
+# ----------------------------------------
+
+# for i in range(0, 10):
+#     if i >= 5:
+#         break
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(0, 11):
+#     if i == 3:
+#         continue
+#     if i == 7:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+
+# total = 0
+
+# for i in range(1, 21):
+#     total += i
+# print(total)
+
+# ----------------------------------------
+
+# total = 0
+
+# for i in range(0, 31, 5):
+#     total += i
+
+# print(total)
+
+# ----------------------------------------
+
+# for row in range(1, 4):
+#     for col in range(1, 4):
+#         print(f'row: {row}, col: {col}')
+
+# ----------------------------------------
+
+# age = int(input("How old are you?"))
+# if age >= 20:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# ----------------------------------------
+
+# while True:
+#     Check = input("Do you want to pay Y or N?")
+#     if Check == "Y":
+#         print("Yes")
+#         break
+#     elif Check == "N":
+#         print("No")
+#         break
+#     else:
+#         print("Wrong input")
+
+# ----------------------------------------
+
+# for number in range(1, 11):
+#     print(f'number: {number}')
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+
+# for letter in range(8):
+#     print("Practice")
+
+# ----------------------------------------
+
+# for i in range(1, 13):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(5, 26):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(1, 20, 2):
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(20, 9, -1):
+#     print(i)
+
+# ----------------------------------------
+
+# name = "MINUK"
+
+# for i in name:
+#     print(i)
+
+# ----------------------------------------
+
+# foods = ["ramen", "sushi", "burger", "pizza"]
+
+# for food in foods:
+#     print(food)
+
+# ----------------------------------------
+
+# for i in range(0, 11):
+#     if i >= 6:
+#         break
+#     print(i)
+
+# ----------------------------------------
+
+# for i in range(0, 11):
+#     if i == 2:
+#         continue
+#     if i == 5:
+#         continue
+#     if i == 8:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+
+# total = 0
+# for i in range(1, 51):
+#     total += i
+# print(total)
+
+# ----------------------------------------
+
+# total = 0
+
+# for i in range(0, 41, 4):
+#     total += i
+# print(total)
+
+# ----------------------------------------
+
+# for row in range(5):
+#     for col in range(3):
+#         print(f'row: {row}, col: {col}')
+
+# ----------------------------------------
+
+# score = input('put in your score')
+
+# if int(score) >= 60:
+#     print('Pass')
+
+# elif int(score) < 60:
+#     print('Fail')
+
+# ----------------------------------------
+
+# answer = 'python123'
+
+# password = input('press your pass word')
+# if password == answer:
+#     print('Login success')
+# else:
+#     print('Wrong password')
+
+# ----------------------------------------
+
+# for i in range(1, 6):
+#     print(f'count: {i}')
+
+# ============================================================================================================================
+# 1.
+# for letter in range(3):
+#     print('Python Study')
+
+# ----------------------------------------
+# 2.
+# for i in range(1, 16):
+#     print(i)
+
+# ----------------------------------------
+# 3.
+# for i in range(8, 19):
+#     print(i)
+
+# ----------------------------------------
+# 4.
+# for i in range(1, 31, 2):
+#     print(i)
+
+# ----------------------------------------
+# 5.
+# for i in range(30, 19, -1):
+#     print(i)
+
+# ----------------------------------------
+# 6.
+# letter = "TOKYO"
+
+# for i in letter:
+#     print(i)
+
+# ----------------------------------------
+# 7.
+# animals = ['cat', 'dog', 'bird', 'tigger']
+# for animal in animals:
+#     print(animal)
+
+# ----------------------------------------
+# 8.
+# for i in range(0, 13):
+#     if i == 8:
+#         break
+#     print(i)
+
+# ----------------------------------------
+# 9.
+# for i in range(0, 13):
+#     if i == 4:
+#         continue
+#     if i == 6:
+#         continue
+#     if i == 10:
+#         continue
+#     print(i)
+
+# ----------------------------------------
+# 10.
+# count = 0
+
+# for i in range(1, 101):
+#     count = count + i
+# print(count)
+
+# ----------------------------------------
+# 11.
+# count = 0
+# for i in range(5, 51, 5):
+#     count = count + i
+# print(count)
+
+# ----------------------------------------
+# 12.
+# for row in range(1, 5):
+#     for col in range(1, 4):
+#         print(f'row: {row}, col: {col}')
+
+# ----------------------------------------
+# 13.
+# age = int(input('type your age'))
+
+# if age >= 18:
+#     print('Can enter')
+# else:
+#     print('Cannot enter')
+
+# ----------------------------------------
+# # 14.
+# number = int(input('type any number'))
+
+# if number > 10:
+#     print('Big')
+# else:
+#     print('Small')
+
+# ----------------------------------------
+# # 15.
+# for i in range(1, 8):
+#     print(f'day: {i}')
